02fad689057b4775c28affc7e569e90d09f0caf7
sudheer393 <sudheer.kandukuri@qentelli.com>
2025-12-31 17:49:14 +0530

    Update Variablest.js

--- Variablest.js
+++ Variablest.js
@@ -1,4 +1,49 @@
-let a= "Hello"
-let b= "World"
-const add = a+b
-console.log(add);
+//Addition
+
+let a= 1
+let b= 2
+const addition = a+b
+console.log('addition'+ addition);
+
+
+//Substraction 
+
+let c=10
+let d=20
+const sub = d-c
+console.log('Substraction'+ sub);
+
+//multiplication
+
+let e=10
+let f=20
+const mul = e*f
+console.log('multiplication'+ mul);
+
+//Division
+
+let x=100
+let y=5
+const div = x/y
+console.log('Division'+ div);
+
+//Area of circle 
+
+let Pi= 3.14
+let radius = 2
+const areaofcircle = Pi*radius*radius
+console.log('Area of circle'+areaofcircle);
+
+//Area of cylinder
+let PI= 3.14
+let Radius = 4
+let height = 8
+const cylinder = 2*3.14*Radius*height+2*3.14*Radius*Radius
+console.log('Area of cylinder'+ cylinder);
+
+//Area of Rectangle
+
+let w= 10
+let l=15
+const Rectangle = w*l
+console.log('Area of Rectangle'+ Rectangle);
